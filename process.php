613f3458ec474e198e23d847b98c67610630e1a0
Kanokkot <89740283+Kanokkot@users.noreply.github.com>
2021-09-27 14:23:16 +0700

    Update process.php

--- process.php
+++ process.php
@@ -1,14 +1,27 @@
-<html>
-  <head>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <form action="process.php" method="GET">
-      คะแนน : 
-      <input type="text" name="data1" />
-      <input type="submit" value="ยืนยัน" />
-    </form>
-  </body>
-</html>
-
-
+<?php
+    $score = $_GET["data1"];
+    if ($score >= 80) {
+        echo "Your grade is A.";
+    } 
+     else if ($score >=75) {
+        echo "Your grade is B+.";
+    } 
+    else if ($score >= 70) {
+        echo "Your grade is B.";
+    } 
+     else if ($score >=65) {
+        echo "Your grade is C+.";
+    } 
+    else if ($score >= 60) {
+        echo "Your grade is C.";
+    }
+     else if ($score >=55) {
+        echo "Your grade is D+ .";
+    } 
+     else if ($score >=51) {
+        echo "Your grade is D.";
+    } 
+    else {
+        echo "Sorry, you got grade F.";
+    }
+?>
