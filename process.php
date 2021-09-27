18b9ca5c910c4b72e7c881bd6bac869aa174783e
Kanokkot <89740283+Kanokkot@users.noreply.github.com>
2021-09-27 14:21:51 +0700

    Update process.php

--- process.php
+++ process.php
@@ -1,3 +1,14 @@
-<?php
-  echo $_GET['data'];
-?>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <form action="process.php" method="GET">
+      คะแนน : 
+      <input type="text" name="data1" />
+      <input type="submit" value="ยืนยัน" />
+    </form>
+  </body>
+</html>
+
+
