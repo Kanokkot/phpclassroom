c4f5cc84482df498a874238006a0c6dd24071e56
Kanokkot <89740283+Kanokkot@users.noreply.github.com>
2021-11-19 14:46:21 +0700

    Update tableloop1.php

--- tableloop1.php
+++ tableloop1.php
@@ -6,11 +6,14 @@
       <?php
         for($i=0;$i<100;$i++){
           echo "<tr>";
-          echo "<td>กนกกช</td>";
+          echo "<td>กนกกช 1</td>";
+          echo "<td>กนกกช 2</td>";
+          echo "<td>กนกกช 3</td>";
+          echo "<td>กนกกช 4</td>";
+          echo "<td>กนกกช 5</td>";
+          echo "<td>กนกกช 6</td>";
           echo "</tr>";
-          echo "<tr>";
-          echo "<td>กนกกช</td>";
-          echo "</tr>";
+        
         }
       ?>
     </table>
