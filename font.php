a4851c8bba8407e47eea5d7b0d8aa82e7313b8a0
Kanokkot <89740283+Kanokkot@users.noreply.github.com>
2022-02-11 14:24:48 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,29 @@
+<html>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: myFont;
+        src: url(shining.woff);
+      }
+      @font-face{
+        font-family: myFont2;
+        src: url(.....);
+      }
+      #font1{
+        font-family: myFont;
+      }
+      #font2{
+        font-family: myFont2;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="font1">
+      <h1>นายยิ่งคุณ รอดทิม เลขที่ 35 ห้อง 5/13</h1>
+    </div>
+    <div id="font2">
+      <h1>นายยิ่งคุณ รอดทิม เลขที่ 35 ห้อง 5/13</h1>
+    </div>
+  </body>
+</html>
