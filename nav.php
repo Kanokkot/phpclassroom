338ab1493c266317923a0a2605ea10fa6838a888
Kanokkot <89740283+Kanokkot@users.noreply.github.com>
2022-02-11 14:11:22 +0700

    Update nav.php

--- nav.php
+++ nav.php
@@ -2,17 +2,25 @@
   <head>
     <title>Navigation Bar</title>
     <style>
-      ul {
-        list-style-type: none;
+            ul {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+      overflow: hidden;
+      background-color: #333;
       }
-      li a {
-        display: block;
-        padding: 8px 16px;
-        text-decoration: none;
+            li {
+            float: left;
+            }
+            li a {
+      display: block;
+      color: white;
+      text-align: center;
+      padding: 14px 16px;
+      text-decoration: none;
       }
       li a:hover {
-        background-color: #555;
-        color: white;
+      background-color: #111;
       }
     </style>
   </head>
