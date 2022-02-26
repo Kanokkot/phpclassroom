379fa55cc946fee71427efeb6d5d77fd5dab2bce
Kanokkot <89740283+Kanokkot@users.noreply.github.com>
2022-02-26 23:36:28 +0700

    Update font.php

--- font.php
+++ font.php
@@ -1,29 +1,9 @@
-<html>
-  <head>
-    <title>การกำหนดรูปแบบตัวอักษร</title>
-    <style>
-      @font-face{
-        font-family: myFont;
-        src: url(shining.woff);
-      }
-      @font-face{
-        font-family: myFont2;
-        src: url(.....);
-      }
-      #font1{
-        font-family: myFont;
-      }
-      #font2{
-        font-family: myFont2;
-      }
-    </style>
-  </head>
-  <body>
-    <div id="font1">
-      <h1>นายยิ่งคุณ รอดทิม เลขที่ 35 ห้อง 5/13</h1>
-    </div>
-    <div id="font2">
-      <h1>นายยิ่งคุณ รอดทิม เลขที่ 35 ห้อง 5/13</h1>
-    </div>
-  </body>
-</html>
+<style>
+@font-face {
+font-family: myFirstFont;
+src: url(sansation_light.woff);
+}
+body{
+font-family: myFirstFont;
+}
+<style>
