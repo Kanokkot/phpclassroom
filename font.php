e73ed33f5996b6fe524ca67e56a37a422f750e00
Kanokkot <89740283+Kanokkot@users.noreply.github.com>
2022-03-03 04:56:49 +0700

    Update font.php

--- font.php
+++ font.php
@@ -1,9 +1,29 @@
-<style>
-@font-face {
-  font-family: myFirstFont;
-  src: url(sansation_light.woff);
-}
-body{
-  font-family: myFirstFont;
-}
-<style>
+<html>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: myFont;
+        src: url(DSNFH__.woff);
+      }
+      @font-face{
+        font-family: myFont2;
+        src: url(2547_Ddinya-08N.woff);
+      }
+      #font1{
+        font-family: myFont;
+      }
+      #font2{
+        font-family: myFont2;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="font1">
+      <h1>นางสาวกนกกช  ดอกหอม เลขที่ 13 ม.5/13</h1>
+    </div>
+    <div id="font2">
+      <h1>นางสาวกนกกช  ดอกหอม เลขที่ 13 ม.5/13</h1>
+    </div>
+  </body>
+</html>
